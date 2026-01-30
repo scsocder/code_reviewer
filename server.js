437cc99bb7059d3716aee0756b485b9e6a531498
scsocder <shubamchakraborty1997@gmail.com>
2026-01-31 01:30:02 +0530

    backend

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const app = require('./src/app');
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port http://localhost:3000');
+});
+
+exports = app;
---
+++ temp.js
@@ -0,0 +1,4 @@
+function SubmitEvent(){
+    return a + b;
+}
+
